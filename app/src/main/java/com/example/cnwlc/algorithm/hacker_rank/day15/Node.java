81ad05cd2fb1acf929ab392a0f349a3633585c33
JiCheol Choo <jicheolchoo@gmail.com>
2020-04-13 10:20:55 +0900

    Add day15 algorithm
    Signed-off-by: JiCheol Choo <jicheolchoo@gmail.com>

---
+++ app/src/main/java/com/example/cnwlc/algorithm/hacker_rank/day15/Node.java
@@ -0,0 +1,11 @@
+package com.example.cnwlc.algorithm.hacker_rank.day15;
+
+public class Node {
+    public int data;
+    public Node next;
+
+    public Node(int d) {
+        data = d;
+        next = null;
+    }
+}
