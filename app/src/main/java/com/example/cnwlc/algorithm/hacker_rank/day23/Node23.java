1fa1c9844083daea9338a3b15ef4fb24506d35d3
JiCheol Choo <jicheolchoo@gmail.com>
2020-04-20 11:46:48 +0900

    Add day 23 algorithm
    Signed-off-by: JiCheol Choo <jicheolchoo@gmail.com>

---
+++ app/src/main/java/com/example/cnwlc/algorithm/hacker_rank/day23/Node23.java
@@ -0,0 +1,10 @@
+package com.example.cnwlc.algorithm.hacker_rank.day23;
+
+public class Node23 {
+    Node23 left,right;
+    int data;
+    Node23(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
