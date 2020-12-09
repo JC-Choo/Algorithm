3a7fdaf0e9b07525e162e3e0a56b915cffa00822
DESKTOP-0HPCUNM\cnwlc <cnwlcjf1@naver.com>
2020-12-09 22:09:45 +0900

    이진 트리 추가
    Signed-off-by: DESKTOP-0HPCUNM\cnwlc <cnwlcjf1@naver.com>

--- app/src/test/java/com/example/cnwlc/algorithm/fast/DataStructure.kt
+++ app/src/test/java/com/example/cnwlc/algorithm/fast/DataStructure.kt
@@ -1,4 +1,24 @@
 package com.example.cnwlc.algorithm.fast
 
+import org.junit.Test
+
 class DataStructure {
+    @Test
+    fun 이진트리() {
+        val tree = BinaryTree()
+        tree.insertNode(5)
+        tree.insertNode(8)
+        tree.insertNode(7)
+        tree.insertNode(10)
+        tree.insertNode(9)
+        tree.insertNode(11)
+
+//        if (tree.removeNode(10)) {
+//            println("노드 삭제")
+//        }
+
+//         tree.inorderTree(tree.rootNode, 0);
+         tree.postorderTree(tree.rootNode, 0)
+//        tree.preorderTree(tree.rootNode, 0)
+    }
 }
