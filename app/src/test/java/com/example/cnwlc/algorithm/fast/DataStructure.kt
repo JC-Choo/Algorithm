6d8afd290346cd9bf5432770c0c61a48d15b6dac
DESKTOP-0HPCUNM\cnwlc <cnwlcjf1@naver.com>
2020-12-10 22:47:00 +0900

    Test binary tree
    Signed-off-by: DESKTOP-0HPCUNM\cnwlc <cnwlcjf1@naver.com>

--- app/src/test/java/com/example/cnwlc/algorithm/fast/DataStructure.kt
+++ app/src/test/java/com/example/cnwlc/algorithm/fast/DataStructure.kt
@@ -7,18 +7,21 @@ class DataStructure {
     fun 이진트리() {
         val tree = BinaryTree()
         tree.insertNode(5)
-        tree.insertNode(8)
         tree.insertNode(7)
-        tree.insertNode(10)
-        tree.insertNode(9)
-        tree.insertNode(11)
+        tree.insertNode(8)
+        tree.insertNode(3)
+        tree.insertNode(2)
+        tree.insertNode(6)
+//        tree.insertNode(4)
 
-//        if (tree.removeNode(10)) {
-//            println("노드 삭제")
-//        }
+        println()
+        if (tree.removeNode(3)) {
+            println("노드 삭제")
+        }
+        println()
 
-//         tree.inorderTree(tree.rootNode, 0);
-         tree.postorderTree(tree.rootNode, 0)
+        tree.inorderTree(tree.rootNode, 0);
+//        tree.postorderTree(tree.rootNode, 0)
 //        tree.preorderTree(tree.rootNode, 0)
     }
 }
