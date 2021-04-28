158fd7912a591ffdb24b102cef8b9d74713e0bed
JC-Choo <jicheolchoo@gmail.com>
2021-04-28 22:46:11 +0900

    Add problems up to 10992
    Signed-off-by: JC-Choo <jicheolchoo@gmail.com>

--- app/src/main/java/com/example/cnwlc/algorithm/baekjoon/1.입출력.kt
+++ app/src/main/java/com/example/cnwlc/algorithm/baekjoon/1.입출력.kt
@@ -1,5 +1,6 @@
 package com.example.cnwlc.algorithm.baekjoon
 
+import android.util.Log
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
@@ -10,7 +11,7 @@ import java.util.*
 object 입출력 {
     @JvmStatic
     fun main(args: Array<String>) {
-        p10818()
+        p10992()
     }
 
     fun p2557() {
@@ -50,7 +51,7 @@ object 입출력 {
         while (sc.hasNextInt()) {
             val a = sc.nextInt()
             val b = sc.nextInt()
-            println(a+b)
+            println(a + b)
         }
     }
 
@@ -60,9 +61,8 @@ object 입출력 {
             val a = sc.nextInt()
             val b = sc.nextInt()
 
-            if (a == 0 && b == 0)
-                break
-            println(a+b)
+            if (a == 0 && b == 0) break
+            println(a + b)
         }
     }
 
@@ -72,7 +72,7 @@ object 입출력 {
         for (i in 0 until testCase.toInt()) {
             val line = sc.nextLine()
             val number = line.split(",")
-            println(number[0].toInt()+number[1].toInt())
+            println(number[0].toInt() + number[1].toInt())
         }
     }
 
@@ -81,7 +81,7 @@ object 입출력 {
         val testCase = sc.nextLine()
         for (i in 0 until testCase.toInt()) {
             val number = sc.nextLine().split(" ")
-            println("Case #${i+1}: ${number[0].toInt()+number[1].toInt()}")
+            println("Case #${i + 1}: ${number[0].toInt() + number[1].toInt()}")
         }
     }
 
@@ -92,7 +92,7 @@ object 입출력 {
             val number = sc.nextLine().split(" ")
             val a = number[0].toInt()
             val b = number[1].toInt()
-            println("Case #${i+1}: $a + $b = ${a+b}")
+            println("Case #${i + 1}: $a + $b = ${a + b}")
         }
     }
 
@@ -144,7 +144,7 @@ object 입출력 {
     fun p2741() {
         val sc = Scanner(System.`in`)
         val data = sc.nextInt()
-        for (i in 1 .. data) {
+        for (i in 1..data) {
             println(i)
         }
     }
@@ -160,8 +160,8 @@ object 입출력 {
     fun p2739() {
         val sc = Scanner(System.`in`)
         val data = sc.nextInt()
-        for (i in 1 .. 9) {
-            println("$data * $i = ${data*i}")
+        for (i in 1..9) {
+            println("$data * $i = ${data * i}")
         }
     }
 
@@ -180,7 +180,7 @@ object 입출력 {
         }
         sum += day
 
-        println(days[sum%7])
+        println(days[sum % 7])
     }
 
     fun p8393() {
@@ -205,6 +205,221 @@ object 입출력 {
 
         val result = list.sorted()
 
-        println(""+result[0]+" "+result[result.size-1])
+        println("" + result[0] + " " + result[result.size - 1])
+    }
+
+    fun p2438() {
+        val sc = Scanner(System.`in`)
+        val n = sc.nextInt()
+        for (i in 0 until n) {
+            for (j in 0 until i + 1) {
+                print("*")
+            }
+            println()
+        }
+    }
+
+    fun p2439() {
+        val sc = Scanner(System.`in`)
+        val n = sc.nextInt()
+        for (i in 0 until n) {
+            for (j in 0 until n - i - 1) {
+                print(" ")
+            }
+            for (j in 0 until i + 1) {
+                print("*")
+            }
+
+            println()
+        }
+    }
+
+    fun p2440() {
+        val sc = Scanner(System.`in`)
+        val n = sc.nextInt()
+        for (i in 0 until n) {
+            for (j in 0 until n - i) {
+                print("*")
+            }
+
+            println()
+        }
+    }
+
+    fun p2441() {
+        val sc = Scanner(System.`in`)
+        val n = sc.nextInt()
+        for (i in 0 until n) {
+            for (j in 0 until i) {
+                print(" ")
+            }
+
+            for (j in i until n) {
+                print("*")
+            }
+
+            println()
+        }
+    }
+
+    fun p2442() {
+        val sc = Scanner(System.`in`)
+        val n = sc.nextInt()
+        for (i in 0 until n) {
+            for (j in i until n - 1) {
+                print(" ")
+            }
+
+            for (j in n - i..n + i) {
+                print("*")
+            }
+            println()
+        }
+    }
+
+    fun p2445() {
+        val sc = Scanner(System.`in`)
+        val n = sc.nextInt()
+        for (i in 0 until 2 * n - 1) {
+            if (i < n) {
+                for (j in 0 until i + 1) {
+                    print("*")
+                }
+
+                for (j in i + 1 until 2 * n - i - 1) {
+                    print(" ")
+                }
+
+                for (j in 2 * n - i until 2 * n + 1) {
+                    print("*")
+                }
+            } else {
+                val k = i - n
+                for (j in 0 until n - k - 1) {
+                    print("*")
+                }
+
+                for (j in n - k..n + k + 1) {
+                    print(" ")
+                }
+
+                for (j in n + k until 2 * n - 1) {
+                    print("*")
+                }
+            }
+            println()
+        }
+        println("=============")
+    }
+
+    fun p2522() {
+        val sc = Scanner(System.`in`)
+        val n = sc.nextInt()
+        for (i in 0 until 2 * n - 1) {
+            if (i < n) {
+                for (j in 0 until n - i - 1) {
+                    print(" ")
+                }
+                for (j in n - i - 1 until n) {
+                    print("*")
+                }
+            } else {
+                val k = i - n + 1
+                for (j in 0 until k) {
+                    print(" ")
+                }
+                for (j in k until n) {
+                    print("*")
+                }
+            }
+            println()
+        }
+        println("=============")
+    }
+
+    fun p2446() {
+        val sc = Scanner(System.`in`)
+        val n = sc.nextInt()
+        for (i in 0 until 2 * n - 1) {
+            if (i < n) {
+                for (j in 0 until i) {
+                    print(" ")
+                }
+
+                for (j in i until 2 * n - 1 - i) {
+                    print("*")
+                }
+            } else {
+                val k = i - n
+                for (j in 0 until n - k - 2) {
+                    print(" ")
+                }
+
+                for (j in n - k until n + k + 3) {
+                    print("*")
+                }
+            }
+
+            println()
+        }
+        println("=============")
+    }
+
+    fun p10991() {
+        val sc = Scanner(System.`in`)
+        val n = sc.nextInt()
+        for (i in 0 until n) {
+            var str = ""
+            for (j in 0 until n - i - 1) {
+                str += " "
+            }
+            for (j in n - i - 1 until n + i + 1) {
+                str += if (n % 2 == 0) {
+                    if (i % 2 == 0) {
+                        if (j % 2 == 0) " " else "*"
+                    } else {
+                        if (j % 2 == 0) "*" else " "
+                    }
+                } else {
+                    if (i % 2 == 0) {
+                        if (j % 2 == 0) "*" else " "
+                    } else {
+                        if (j % 2 == 0) " " else "*"
+                    }
+                }
+            }
+
+            println(str.substring(0, str.length - 1))
+        }
+        println("=============")
+    }
+
+    fun p10992() {
+        val sc = Scanner(System.`in`)
+        val n = sc.nextInt()
+        for (i in 0 until n - 1) {
+            for (j in 0 until n - i - 1) {
+                print(" ")
+            }
+            if (i == 0) print("*")
+            else {
+                for (j in n - i - 1 until n + i) {
+                    if (j == n-i-1) {
+                        print("*")
+                    } else if (j == n+i-1) {
+                        print("*")
+                    } else {
+                        print(" ")
+                    }
+                }
+            }
+
+            println()
+        }
+        for (i in 0 until 2*n-1) {
+            print("*")
+        }
+        println()
+        println("=============")
     }
 }
